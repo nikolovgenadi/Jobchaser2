bc204ecaa54bed3487ca705bdabb6894ccfe6ae9
Genadi Nikolov <genadi123.nik@gmail.com>
2024-08-26 16:06:41 +0200

    bootstrap navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import List from "./components/list";
+import NavbarComponent from "./components/navbar";
 
 function App(): JSX.Element {
   return (
     <div className="App">
+      <NavbarComponent />
       <h1>Job Chaser</h1>
       <List />
     </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,3 +7,32 @@ import {
   FormControl,
   Button,
 } from "react-bootstrap";
+
+const NavbarComponent = () => (
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link href="#" disabled>
+            Disabled
+          </Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+export default NavbarComponent;
